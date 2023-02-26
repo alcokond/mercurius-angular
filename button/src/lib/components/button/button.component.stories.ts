0734d569c7a4217a51420f60d80c8bd954af54ef
Alan Coello <alancoello.12@gmail.com>
2023-02-26 03:45:13 -0500

    story

--- button/src/lib/components/button/button.component.stories.ts
+++ button/src/lib/components/button/button.component.stories.ts
@@ -1,17 +1,32 @@
 import { moduleMetadata, Story, Meta } from '@storybook/angular';
 import { MercButtonModule } from '../../button.module';
+import { MercButtonComponent } from './button.component';
+
 
 export default {
   title:'Button',
 };
 
-export const primary = () => ({
+const Template : Story<MercButtonComponent> = (args : MercButtonComponent) => ({
   moduleMetadata: {
     imports: [MercButtonModule],
 
   },
   template: `
-  <button merc-button>Primary</button>
+  <button merc-button [color]="color">Button</button>
   `,
-  props: {},
+  props: {
+    ...args
+  }
 });
+
+export const primary = Template.bind({});
+
+primary.args ={
+  color: 'primary'
+}
+export const secondary = Template.bind({});
+
+secondary.args ={
+  color: 'secondary'
+}
--- button/src/lib/components/button/button.component.ts
+++ button/src/lib/components/button/button.component.ts
@@ -1,11 +1,17 @@
-import { ChangeDetectionStrategy, Component, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, ViewChild } from '@angular/core';
+import { primary } from './button.component.stories';
 
 @Component({
   selector: '[merc-button]',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    '[class.merc-primary]': 'color === "primary"',
+    '[class.merc-secondary]': 'color === "secondary"',
+  }
 })
 export class MercButtonComponent {
 
+  @Input() color : 'primary' | 'secondary' = 'primary';
 }
